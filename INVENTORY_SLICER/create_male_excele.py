cc410b456da14efd6720e7364ae135bd3c7fb656
matqsarlab <m.balicki@qsarlab.com>
2024-08-29 13:20:00 +0200

    update

--- INVENTORY_SLICER/create_male_excele.py
+++ INVENTORY_SLICER/create_male_excele.py
@@ -2,13 +2,14 @@ import pandas as pd
 from func_tools import pipleine2excels
 
 df1 = pd.read_excel(
-    "~/Downloads/NAMs_inventory_v20240723-A-L.xlsx",
+    "./NAMs_inventory_v20240723-A-L.xlsx",
     sheet_name="mynamms",
 )
 df2 = pd.read_excel(
-    "~/Downloads/NAMs_inventory_v20240723_M-Z.xlsx",
+    "./NAMs_inventory_v20240723_M-Z.xlsx",
     sheet_name="mynamms",
 )
 df2.columns = df1.columns
 inventory = pd.concat([df1, df2], ignore_index=True)
 pipleine2excels(inventory)
+print("Done!")
--- INVENTORY_SLICER/standardize_name.py
+++ INVENTORY_SLICER/standardize_name.py
@@ -2,13 +2,14 @@ import pandas as pd
 from func_tools import cleaner_chain
 
 df1 = pd.read_excel(
-    "~/Downloads/NAMs_inventory_v20240723-A-L.xlsx",
+    "./NAMs_inventory_v20240723-A-L.xlsx",
     sheet_name="mynamms",
 )
 df2 = pd.read_excel(
-    "~/Downloads/NAMs_inventory_v20240723_M-Z.xlsx",
+    "./NAMs_inventory_v20240723_M-Z.xlsx",
     sheet_name="mynamms",
 )
 df2.columns = df1.columns
 inventory = pd.concat([df1, df2], ignore_index=True)
 cleaner_chain(inventory, "first_stage.xlsx")
+print("Done!")
